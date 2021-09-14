ac7cd021bbfd7e924ae9b9cbd52ccd2eef10a91f
Peter  C Rigby <rigbypc@gmail.com>
2021-09-14 12:02:46 -0400

    Getting the code under test

--- src/test/java/soen345/AppTest.java
+++ src/test/java/soen345/AppTest.java
@@ -1,20 +1,24 @@
 package soen345;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+
+import java.util.Stack;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void stackTest()
     {
-        assertTrue( true );
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+
+        assertEquals(2, stack.pop().intValue());
     }
 }
